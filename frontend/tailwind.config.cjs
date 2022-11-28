57ebf9a756cc21c8d51fc6032a0e20a6b12acf94
Topi99 <topiltzin.hernandez@wizeline.com>
2022-11-27 20:10:20 -0600

    chore: basic full layout

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      ...colors,
       purple: {
         100: "#352559",
         200: "#B963F2",
@@ -13,7 +14,6 @@ module.exports = {
         400: "#190E40",
       },
       white: "#F2F2F2",
-      gray: colors.gray,
     },
   },
   plugins: [],
